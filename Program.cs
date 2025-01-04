3dce90b347f9e05839b2fac2884dfe3214c24890
Miguel Arismendi <miguel@biofile.com.co>
2025-01-04 13:20:47 -0500

    Refactoring: Delete calls using thath not use the project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.Xml;
 using System.Text.RegularExpressions;
-using static System.Net.Mime.MediaTypeNames;
+
 
 namespace Replace_Code_FrontBack
 {
